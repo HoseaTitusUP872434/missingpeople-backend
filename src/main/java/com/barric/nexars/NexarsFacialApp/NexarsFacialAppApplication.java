b4901939749035083f3c293a1ecadc436b9c2fff
Eric <30669026+educare-mit@users.noreply.github.com>
2019-03-15 13:33:13 +0100

    after login implementation

--- src/main/java/com/barric/nexars/NexarsFacialApp/NexarsFacialAppApplication.java
+++ src/main/java/com/barric/nexars/NexarsFacialApp/NexarsFacialAppApplication.java
@@ -2,27 +2,50 @@ package com.barric.nexars.NexarsFacialApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableJpaRepositories(basePackages = "com.barric.nexars.NexarsFacialApp.repositories")
 @SpringBootApplication
+@Configuration
 public class NexarsFacialAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NexarsFacialAppApplication.class, args);
     }
-
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**").allowedOrigins("http://localhost:8102/")
+//                         .allowedMethods("PUT", "DELETE","POST")
+//			.allowedHeaders("Authorization", "header2", "header3")
+//		.exposedHeaders("Authorization", "header2");
+//            }
+//        };
+//    }
+    
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8100");
-            }
-        };
-    }
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 }
